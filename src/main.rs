e37f939fb59b9f5ddd40a22a6d6fcf33ba9575e6
NickSpyker <nicolas.spijkerman@epitech.eu>
2022-02-14 13:00:34 +0100

    add info

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,6 @@ use std::fs;
 use chrono;
 
 fn main() -> Result<(), Box<dyn Error>> {
-
     // HTML request historic
     let data = reqwest::blocking::get("https://bourse.boursorama.com/bourse/action/graph/ws/GetTicksEOD?symbol=1rPRNO&length=1&period=0&guid=")?.text()?;
     println!("HTML request - Data OK !");
@@ -33,11 +32,14 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Start main loop
     while true {
+        // HTML request recenr data
         print!("\tRequest: ");
         buffer = reqwest::blocking::get("https://bourse.boursorama.com/bourse/action/graph/ws/UpdateCharts?symbol=1rPRNO&period=-1")?.text()?;
         print!("OK !");
+        // Write into out.json
         fs::write("out.json", buffer).expect("Unable to write file");
         println!(" -> out.json loaded | {:?}", chrono::offset::Local::now());
+        // Sleep for [x] secs
         thread::sleep(Duration::from_secs(60));
     }
     Ok(())
